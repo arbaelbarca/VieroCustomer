991d693a8c8cd16ba5595bfcd144e6b8ed196f01
CindoddCindy <cindodcindy@gmail.com>
2021-06-20 12:32:33 +0700

    viero customer layout

---
+++ vieroShoesApp/app/src/main/java/com/cindodcindy/vieroshoes/view/Chat_page.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.vieroshoes.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.vieroshoes.R;
+
+public class Chat_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_page);
+    }
+}
