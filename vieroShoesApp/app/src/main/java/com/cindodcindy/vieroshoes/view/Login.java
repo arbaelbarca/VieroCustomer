169a4b53e5b351f76854997cbf374ac87ba6eb2e
CindoddCindy <cindodcindy@gmail.com>
2021-07-15 00:24:49 +0700

    tambah menu baru

--- vieroShoesApp/app/src/main/java/com/cindodcindy/vieroshoes/view/Login.java
+++ vieroShoesApp/app/src/main/java/com/cindodcindy/vieroshoes/view/Login.java
@@ -24,7 +24,7 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(Login.this,BottomNavActivity.class);
+                Intent intent = new Intent(Login.this,MenuBaruActivity.class);
                 startActivity(intent);
                 finish();
             }
